72336626343d63695b8d588e40980f60dcafdbb1
Klem26 <Kankina26@gmail.com>
2021-01-28 12:48:58 +0200

    Задачи 4-го модуля

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function makePizza() {
+  return 'Ваша пицца готовится, ожидайте.';
+}
+
+
+const result = (makePizza());
+const pointer = (makePizza);
+
+//Дополни код так, чтобы в переменной result был результат выполнения функции makePizza, а в переменной pointer была ссылка на функцию makePizza
---
+++ js/task-5.js
@@ -0,0 +1,19 @@
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+ 
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+    }
+
+    return `Заказ принят, готовим пиццу «${pizzaName}».`;
+  },
+
+};
+console.log(pizzaPalace.order('Венская'))
+console.log(pizzaPalace.order('Четыре нарезона'))
